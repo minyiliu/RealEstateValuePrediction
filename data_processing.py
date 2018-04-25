b04e158c36cdbe11a7171a1f62a9beeddfb1ac44
minyi <minyi@cmu.edu>
2018-04-25 00:42:13 -0400

    [update]select useful columns, todo: proptype-propclass

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv('data/2016-01.csv')
-print(df.shape)
-df = df[df.saleamt != 0]
-print(df.shape)
+for month in range(1,13):
+    filename = 'data/2016-%02d.csv'%month
+    df = pd.read_csv(filename, usecols=['bathstotal', 'beds', 'latitude', 'longitude', 'propclass', 'proptype', 'yearbuilt',
+                 'universalsize', 'saleTransDate', 'saleamt'],
+        parse_dates=['saleTransDate'])
+    # df = df[['bathstotal', 'beds', 'latitude', 'longitude', 'propclass', 'proptype', 'yearbuilt', 'propLandUse',
+    #              'universalsize', 'saleTransDate', 'saleamt']]
+    df = df[(df.saleamt > 0) & (df.universalsize > 0) & (df.yearbuilt > 0)]
+    df.dropna(axis=0, how='any', inplace=True)
+    if month == 1:
+        res = df
+    else:
+        res = res.append(df)
+
+res.loc[:, 'age_year'] = 2016-df.loc[:, 'yearbuilt']
+
+print(res.dtypes)
+
+type = res.loc[:, ['propclass', 'proptype']]
