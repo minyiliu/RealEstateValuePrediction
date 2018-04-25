d419662e343f8e351e3a7c1252c3085e4297dd9e
minyi <minyi@cmu.edu>
2018-04-25 01:02:14 -0400

    [update]wrap as function, download by year

--- sales_downloader.py
+++ sales_downloader.py
@@ -4,38 +4,46 @@
 import requests
 import json
 import csv
+import datetime as dt
 
 # curl -X GET --header 'Accept: application/json' --header 'apikey: 9d078487e223b1c4d54c3f3a3f628803' 'https://search.onboard-apis.com/propertyapi/v1.0.0/sale/snapshot?geoid=CO06075&startsalesearchdate=2016-07-01&endsalesearchdate=2016-09-30'
 
-with open('obDevId') as f:
-    readfile = f.read()
-apikey = readfile.strip()
+def download_data_by_yr(year, geoid='CO06075'):
+    with open('obDevId') as f:
+        readfile = f.read()
+    apikey = readfile.strip()
 
-headers = {}
-headers['apikey'] = apikey
-headers['Accept'] = "application/json"
+    headers = {}
+    headers['apikey'] = apikey
+    headers['Accept'] = "application/json"
 
-hierachy = {}
-resource = "sale"
-package = "snapshot"
-hierachy['resource'] = resource
-hierachy['package'] = package
+    hierachy = {}
+    resource = "sale"
+    package = "snapshot"
+    hierachy['resource'] = resource
+    hierachy['package'] = package
 
-endpoint = "https://search.onboard-apis.com/propertyapi/v1.0.0/{resource}/{package}".format(**hierachy)
+    endpoint = "https://search.onboard-apis.com/propertyapi/v1.0.0/{resource}/{package}".format(**hierachy)
 
-payload = {}
-# cityname = "San Francisco"
-geoid = 'CO06075'
-month = '2016-06'
-startsalesearchdate = '{month}-01'.format(month=month)
-endsalesearchdate = '{month}-30'.format(month=month)
-payload['geoid'] = geoid
-payload['startsalesearchdate'] = startsalesearchdate
-payload['endsalesearchdate'] = endsalesearchdate
-payload['pagesize'] = 10000
+    payload = {}
+    payload['geoid'] = geoid
+    payload['pagesize'] = 10000
 
-r = requests.get(endpoint, params=payload, headers = headers)
+    data_dir = 'data/'
 
-text_file = open(month, "w")
-text_file.write(r.text)
-text_file.close()
+    for month in range(1, 13):
+        startsalesearchdate = str(dt.date(year, month, 1))
+        if month != 12:
+            endsalesearchdate = str(dt.date(year, month + 1, 1) - dt.timedelta(days=1))
+        else:
+            endsalesearchdate = str(dt.date(year, month, 31))
+        payload['startsalesearchdate'] = startsalesearchdate
+        payload['endsalesearchdate'] = endsalesearchdate
+
+        r = requests.get(endpoint, params=payload, headers=headers)
+
+        text_file = open(data_dir + startsalesearchdate[:-3], "w")
+        text_file.write(r.text)
+        text_file.close()
+
+download_data_by_yr(2017)
